605a13a760fcec420f34377ecf33d59909a63311
raj <praj22013@gmail.com>
2024-02-13 12:15:06 +0530

    Active Employee List

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,6 +6,7 @@ import { ExpensesService } from 'src/app/core/services/expenses.service';
 import { VendorService } from 'src/app/core/services/vendor.service';
 import { OfferService } from 'src/app/core/services/offer.service';
 import { MembershipService } from 'src/app/core/services/membership.service';
+import { EmployeeService } from 'src/app/core/services/employee.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,13 +14,69 @@ import { MembershipService } from 'src/app/core/services/membership.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+mpage: any;
+totalRec: any;
+paginateData: any;
+page = 1;
+pageSize = 10;
+collectionSize = 0;
+
+cContact: any;
+  cEmail: any;
+  cName: any;
+  cPoint: any;
+  cPrice: any;
+  cId: any;
+  appId: any;
+totalPriceForDetails: any;
+totalPointForDetails: any;
+  isworking: any;
+modeOfPayment(obj:any) {
+  this.cContact = obj.contact;
+  this.cEmail = obj.email;
+  this.cName = obj.fname + ' ' + obj.lname;
+  this.cPoint = obj.totalpoint;
+  this.cPrice = obj.totalprice;
+  this.cId = obj.custid;
+  this.appId = obj.id;
+}
+openUsedServiceList(obj:any) {
+  this.totalPriceForDetails = obj.totalprice
+  this.totalPointForDetails = obj.totalpoint
+  this.customerService.getServicesListUsingId(obj.id).subscribe((data: any) => {
+    this.servicesList= data;
+    for (let i = 0; i < this.servicesList.length; i++) {
+      this.servicesList[i].index = i + 1;
+    }
+  });
+}
+updateEmpProcessingActive(data:any) {
+  let valu = {
+    id: data.id,
+    isworking: this.isworking = false
+  }
+  this.employeeService.updateEmpActiveStatus(valu).subscribe((data: any) => {
+    this.getAllEmployee();
+  })
+}
+updateEmpProcessingDeActive(data:any) {
+  let valu = {
+    id: data.id,
+    isworking: this.isworking = true
+  }
+  this.employeeService.updateEmpActiveStatus(valu).subscribe((data: any) => {
+    this.getAllEmployee();
+  })
+}
+
 
   // bread crumb items
   breadCrumbItems!: Array<{}>;
   title!: string;
   dataSource!: Object;
   num: number = 0;
-
+  public employeeReg: any[] = [];
+  
   public customerList: any = [];
   public servicesList: any = [];
   public dailyexpensesList: any = [];
@@ -27,9 +84,12 @@ export class DashboardComponent implements OnInit {
   offerList: any = [];
   ExpensesList: any = [];
   MembershipList: any = [];
+appointmentList: any;
+
+ 
 
   constructor(
-
+    private employeeService: EmployeeService,
     private servicesService: ServiceListService,
     private customerService: CustomerService,
     private expensesService: ExpensesService,
@@ -37,9 +97,38 @@ export class DashboardComponent implements OnInit {
     private offerService: OfferService,
     private membershipService: MembershipService,
     private router: Router
-  ) { }
+  ) {
+   
+      this.getAllServices();
+       this.getAllEmployee();
+    
+   }
+   getAllServices() {
+    this.servicesService.getAllServicesList().subscribe((data: any) => {
+      this.servicesList = data;
+      for (let i = 0; i < this.servicesList.length; i++) {
+        this.servicesList[i].index = i + 1;
+      }
+      this.collectionSize = this.servicesList.length;
+       
+    });
+  }
+ 
+  getAllEmployee() {
+    this.employeeService.getAllEmployeeList().subscribe((data: any) => {
+      this.employeeReg = data;
 
-
+      for (let i = 0; i < this.employeeReg.length; i++) {
+        this.employeeReg[i].index = i + 1;
+      }
+      this.collectionSize = this.employeeReg.length;
+      this.getPagintaion();
+    });
+  }
+  getPagintaion() {
+    this.paginateData = this.employeeReg.slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
+    
   option = {
     startVal: this.num,
     useEasing: true,
@@ -53,6 +142,8 @@ export class DashboardComponent implements OnInit {
       { label: 'Dashboard' },
       { label: 'Dashboard', active: true }
     ];
+    
+
     this.getCustomerDetails();
     this.getServicesDetails();
     this.getExpensesDetails();
