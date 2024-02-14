444d7def26305e8b2a78a8385d9c329a7651c105
patelhet20 <het.patel5700@gmail.com>
2024-02-14 11:35:24 +0530

    view saloon module

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SalonRegistartionComponent } from './salon-registartion/salon-registartion.component';
+import { ViewSallonComponent } from './view-sallon/view-sallon.component';
 
 const routes: Routes = [
     {
-        path: 'service-app-salon-registartion',
+        path: 'salon-registartion',
         component: SalonRegistartionComponent
     },
+    {
+        path: 'view-sallon',
+        component: ViewSallonComponent
+    }
 
 ];
 
