44a3d0562d39d82477947eba677e4150f185ea8e
patelhet20 <het.patel5700@gmail.com>
2024-03-14 11:51:00 +0530

    change icons

--- src/app/layouts/sidebar/menu.ts
+++ src/app/layouts/sidebar/menu.ts
@@ -9,55 +9,55 @@ export const MENU: MenuItem[] = [
   {
     id: 2,
     label: 'MENUITEMS.DASHBOARDS.TEXT',
-    icon: 'home',
+    icon: 'mdi mdi-monitor-dashboard',
     link: '/',
   },
   {
     id: 3,
     label: 'Customer List',
-    icon: 'users',
+    icon: 'mdi mdi-account-group',
     link: '/custom/user-list',
   },
   {
     id: 4,
     label: 'Service List',
-    icon: 'list',
+    icon: 'mdi mdi-playlist-check',
     link: '/custom/service-list',
   },
   {
     id: 5,
     label: 'Employee List',
-    icon: 'users',
+    icon: 'mdi mdi-account-multiple',
     link: '/custom/employee-list',
   },
   {
     id: 6,
     label: 'Expense',
-    icon: 'clipboard',
+    icon: 'mdi mdi-finance',
     link: '/custom/expenses',
   },
   {
     id: 7,
     label: 'Combo Offer',
-    icon: 'gift',
+    icon: 'mdi mdi-sale',
     link: '/custom/combo-offer',
   },
   {
     id: 8,
     label: 'Membership',
-    icon: 'gift',
+    icon: 'mdi mdi-wallet-membership',
     link: '/custom/membership',
   },
   {
     id: 9,
     label: 'Vendor',
-    icon: 'anchor',
+    icon: 'mdi mdi-cart',
     link: '/custom/vendor',
   },
   {
-    id:10,
-    label:'banners',
-    icon:'share',
-    link:'/custom/banners'
+    id: 10,
+    label: 'banners',
+    icon: 'mdi mdi-image-area',
+    link: '/custom/banners'
   }
 ];
